60ab85ae8a53e064a9466a9281de9b090e64b336
Piravin K C M <piravin.kcm@gmail.com>
2020-12-31 10:16:41 +0530

    to deploy

--- src/components/toTop.tsx
+++ src/components/toTop.tsx
@@ -5,7 +5,7 @@ import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
 const useStyle = makeStyles((theme: Theme) => createStyles({
     toTop: {
         position: "fixed",
-        // display: "none",
+        display: "none",
         bottom: "20px",
         right: "20px",
         cursor: "pointer",
@@ -21,18 +21,28 @@ const useStyle = makeStyles((theme: Theme) => createStyles({
         height: "50px",
         borderRadius: "50%",
         boxShadow: "5px 5px 15px rgba(1, 0, 60,0.3)",
+        transition: "color 0.3s ease",
         '&:hover': {
             color: "#15de02",
             trnasformOrigin: "bottom",
-            transition: "color 0.5s ease"
+            transition: "color 0.3s ease"
         }
     }
 }));
 
 const ToTop: React.FC = () => {
     const classes = useStyle();
+    const scroller = React.useRef<HTMLDivElement>(null);
+    const goToTop = () => window.scrollTo(0,0);
+    const handleScroll = () => {
+        if(window.pageYOffset > window.innerHeight) scroller.current!.style.display = "block";
+        else scroller.current!.style.display = "none";
+    }
+    React.useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    },[]);
     return(
-        <div className={classes.toTop}>
+        <div id="scroller" className={classes.toTop} ref={scroller} onClick={goToTop}>
             <ArrowUpwardRoundedIcon/>
         </div>
     );
