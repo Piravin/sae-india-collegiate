8352b10f7a3436f69357e30074bff1725eb9850d
Piravin K C M <piravin.kcm@gmail.com>
2020-12-31 10:21:40 +0530

    Optimised

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -5,12 +5,7 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-// M-UI icons
-import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
-// Custom Components
 // Contexts
 import {NavContext} from '../contexts/navContext';
 // Assets
